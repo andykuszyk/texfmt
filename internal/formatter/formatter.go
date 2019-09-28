4e15a1b2859362b552f099de14ae530de77277fe
Andy Kuszyk <pairofsocks@hotmail.co.uk>
2019-09-28 22:17:56 +0100

    WIP towards first test case

--- internal/formatter/formatter.go
+++ internal/formatter/formatter.go
@@ -13,5 +13,33 @@ func Format(file string, width int) (string, error) {
 		return "", errors.New(fmt.Sprintf("Error reading file (%s): %s", file, err))
 	}
 	text := strings.Split(string(fileContents), "\n")
-	return strings.Join(text, "\n"), nil
+	formattedText := []string{}
+	for _, line := range text {
+		for _, splitLine := range splitLines(line, width) {
+			formattedText = append(formattedText, splitLine)
+		}
+	}
+	return strings.Join(formattedText, "\n"), nil
+}
+
+func splitLines(line string, width int) []string {
+	if len(line) <= width {
+		return []string{line}
+	}
+	splitIndex := findSplitIndex(line, width)
+	splitA := line[:splitIndex]
+	splitB := line[splitIndex:]
+	if len(splitB) <= width {
+		return []string{splitA, splitB}
+	} else {
+		return append([]string{splitA}, splitLines(splitB, width)...)
+	}
+}
+
+func findSplitIndex(line string, width int) int {
+	index := width + 1
+	for string(line[index]) != " " {
+		index -= 1
+	}
+	return index + 1
 }
